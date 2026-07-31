d4719209689fa6f7b314f6e93bff0454717c6b29
Okemaelvis <okemaelvis@gmail.com>
2026-07-31 21:04:55 +0300

    Adding API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,7 @@
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
+
+// export default API_URL;
+
+const API_URL = "https://lms-backend-w2r8.onrender.com/api";
 
 export default API_URL;
